fe123a61a98a1fdb6c9378c1c63b502becb05850
donnnq <gemervin14@gmail.com>
2025-08-05 23:18:20 +0800

    🛠️ Scrollchain Update: Added civic glyph protocols, farmer equity system, mood oracle, and EDSA reboot manifesto

--- contracts/vinWorkerLegacySigilRenderer.sol
+++ contracts/vinWorkerLegacySigilRenderer.sol
@@ -1,46 +1,53 @@
-pragma solidity ^0.8.0;
-
 contract vinWorkerLegacySigilRenderer {
-    address public steward;
-
-    struct LegacySigil {
-        address worker;
+    struct Worker {
+        address wallet;
         string name;
-        string[] agencies;
-        string[] badgeTitles;
-        uint256[] ceremonyDates;
-        uint256 cumulativeCaringScore;
+        uint256 performanceScore;
+        string legacyMessage;
+        string sigilHash;
     }
 
-    mapping(address => LegacySigil) public legacyMap;
+    mapping(address => Worker) public workers;
 
-    event LegacySigilCreated(address indexed worker, string name, uint256 caringScore);
-    event SigilUpdated(address indexed worker, string agency, string badgeTitle);
+    event SigilRendered(address indexed worker, string sigilHash);
+    event LegacyUpdated(address indexed worker, string message);
 
-    constructor() {
-        steward = msg.sender;
+    function registerWorker(address _wallet, string memory _name) public {
+        workers[_wallet] = Worker(_wallet, _name, 0, "", "");
     }
 
-    function createSigil(
-        address _worker,
-        string memory _name,
-        string memory _agency,
-        string memory _badgeTitle,
-        uint256 _ceremonyDate,
-        uint256 _caringScore
-    ) public {
-        LegacySigil storage sigil = legacyMap[_worker];
-        sigil.worker = _worker;
-        sigil.name = _name;
-        sigil.agencies.push(_agency);
-        sigil.badgeTitles.push(_badgeTitle);
-        sigil.ceremonyDates.push(_ceremonyDate);
-        sigil.cumulativeCaringScore += _caringScore;
-
-        emit LegacySigilCreated(_worker, _name, sigil.cumulativeCaringScore);
+    function updatePerformance(address _wallet, uint256 _score) public {
+        require(_score <= 100, "Score must be between 0 and 100");
+        workers[_wallet].performanceScore = _score;
     }
 
-    function viewSigil(address _worker) public view returns (LegacySigil memory) {
-        return legacyMap[_worker];
+    function setLegacyMessage(address _wallet, string memory _message) public {
+        workers[_wallet].legacyMessage = _message;
+        emit LegacyUpdated(_wallet, _message);
+    }
+
+    function renderSigil(address _wallet) public {
+        Worker storage w = workers[_wallet];
+        w.sigilHash = generateSigil(w.name, w.performanceScore, w.legacyMessage);
+        emit SigilRendered(_wallet, w.sigilHash);
+    }
+
+    function generateSigil(string memory name, uint256 score, string memory message) internal pure returns (string memory) {
+        return string(abi.encodePacked("SIGIL-", name, "-", uint2str(score), "-", message));
+    }
+
+    function uint2str(uint256 _i) internal pure returns (string memory) {
+        if (_i == 0) return "0";
+        uint256 j = _i;
+        uint256 len;
+        while (j != 0) { len++; j /= 10; }
+        bytes memory bstr = new bytes(len);
+        uint256 k = len;
+        j = _i;
+        while (j != 0) {
+            bstr[--k] = bytes1(uint8(48 + j % 10));
+            j /= 10;
+        }
+        return string(bstr);
     }
 }
