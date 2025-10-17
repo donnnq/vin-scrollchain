bc54d57dfd00178626c52b333cf6ba76896bca60
donnnq <gemervin14@gmail.com>
2025-10-17 16:42:18 +0800

    Seal scrollstorm: Broadcast Immunity Codex + Disaster Ethics DAO + Planetary Civic Broadcast Fellowship for media protection and planetary consequence

---
+++ contracts/BroadcastImmunityCodex.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract BroadcastImmunityCodex {
+    address public admin;
+
+    struct ImmunityEntry {
+        string broadcasterName;
+        string immunityClaim;
+        string emotionalTag;
+        bool summoned;
+        bool granted;
+    }
+
+    ImmunityEntry[] public entries;
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Not authorized");
+        _;
+    }
+
+    function summonImmunity(string memory broadcasterName, string memory immunityClaim, string memory emotionalTag) external onlyAdmin {
+        entries.push(ImmunityEntry(broadcasterName, immunityClaim, emotionalTag, true, false));
+    }
+
+    function grantImmunity(uint256 index) external onlyAdmin {
+        entries[index].granted = true;
+    }
+
+    function getImmunityEntry(uint256 index) external view returns (ImmunityEntry memory) {
+        return entries[index];
+    }
+}
